4446a700fcf057f83645b4861b5655773983511c
Daniel Krol <orblivion@gmail.com>
2015-02-18 11:56:28 -0800

    Add teardown function as well

--- tests.py
+++ tests.py
@@ -5,6 +5,7 @@ from main import generate_files
 
 class WordsTest(unittest.TestCase):
     def setUp(self):
+        # Make sure the expected files don't exist yet
         for fname in ["test_sequences", "test_words"]:
             if os.path.exists(fname):
                 os.remove(fname)
@@ -15,6 +16,12 @@ class WordsTest(unittest.TestCase):
         generate_files([], sequences_fname="test_sequences", words_fname="test_words")
         self.assertTrue(os.path.exists("test_sequences"))
         self.assertTrue(os.path.exists("test_words"))
+
+    def tearDown(self):
+        # So as not to leave a mess
+        for fname in ["test_sequences", "test_words"]:
+            if os.path.exists(fname):
+                os.remove(fname)
 
 if __name__ == '__main__':
     unittest.main()
